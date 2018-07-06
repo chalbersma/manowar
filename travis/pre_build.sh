5518f28da3bea6c47a8f76e3b233c806d4b0c42a
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2018-07-06 15:54:43 -0700

    Moving to a No-SSH test model.

--- travis/pre_build.sh
+++ travis/pre_build.sh
@@ -22,22 +22,6 @@ sudo apt-get update
 # Allow self ssh login
 ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 
-# Authorized Keys
-cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
-
-# Ensure SSHD Enabled
-sudo service ssh restart
-
-cat /etc/ssh/sshd_config
-
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null localhost echo test
-
-# Add Fake Hosts to /etc/hosts
-echo -e "127.0.0.1	testbox1.vrt" | sudo tee -a /etc/hosts
-echo -e "127.0.0.1	testbox5.vrt" | sudo tee -a /etc/hosts
-echo -e "127.0.0.1	testbox4.vrt" | sudo tee -a /etc/hosts
-echo -e "127.0.0.1	testbox3.vrt" | sudo tee -a /etc/hosts
-echo -e "127.0.0.1	testbox2.vrt" | sudo tee -a /etc/hosts
 
 # Make Clean Docs
 rm -rf docs/
