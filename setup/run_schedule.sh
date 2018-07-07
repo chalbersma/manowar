d15f2d7b72761490f11eef7e4a6ec1d404b432f7
Chris Halbersma <chalbersma.12@gmail.com>
2018-07-06 22:22:15 -0700

    Removing references to tunneling.

--- setup/run_schedule.sh
+++ setup/run_schedule.sh
@@ -40,10 +40,6 @@ for i in {8..1} ; do
 	last_loc="${SchedLog}.${i}"
 done
 
-# Check if Tunnelling Desired
-# Disabling Tunnelling EC Specific Thing
-
-
 # Copy the latest to the latest
 cp ${SchedLog} ${SchedLog}.1
 
@@ -58,8 +54,3 @@ ${J2RunLoc}/schedule2.py -b ${CSVFile} -c ${SCHEDConfig} -V &> ${SchedLog}
 
 # Run The Archive Script
 ${J2RunLoc}/collection_archive.py -c ${StoreConfig} -V &>> ${SchedLog}
-
-if [[ ${use_tun} == true ]] ; then
-	# We're using a tunnel so Kill it
-	ssh -S ${TunSocket} -O exit jellyfish@deputy001.bur
-fi
