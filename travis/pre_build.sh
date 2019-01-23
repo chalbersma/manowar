0bd7fedb650382f865b341023250c525f460eafd
Chris Halbersma <chalbersma.12@gmail.com>
2019-01-22 23:22:23 -0800

    Update Pre_Build.sh

--- travis/pre_build.sh
+++ travis/pre_build.sh
@@ -4,9 +4,7 @@
 
 echo -e 'Pre Steps'
 
-echo -e "Enabling Trusty Backports for Shellcheck"
-
-echo -e "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list.d/backports.list
+echo -e "deb http://archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list.d/backports.list
 
 echo -e "Enabling Mariadb 10.2 Repo"
 
@@ -14,7 +12,7 @@ sudo apt-get install software-properties-common
 
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 
-sudo add-apt-repository 'deb [arch=amd64] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu trusty main'
+sudo add-apt-repository 'deb [arch=amd64] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu xenial main'
 
 sudo apt-get update
 
