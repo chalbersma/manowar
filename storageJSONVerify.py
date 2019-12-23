b1aa6b84ec123176bf34bdd3668b5420543c5806
Chris Halbersma <chris@halbersma.us>
2019-12-22 20:36:18 -0800

    Some Updates
    * Storagejsonverify tabs to spaces
    * subtype_large_compare tabs to spaces

--- storageJSONVerify.py
+++ storageJSONVerify.py
@@ -9,76 +9,75 @@ Licensed under the terms of the BSD 2-clause license. See LICENSE file for terms
 import argparse
 # System Operations
 import sys
-# Import Schema Parser
-# Requires python3-jsonschema installed
-import jsonschema
 # Import
 import json
 # Import
 import os
 
-JSONSCHEMA_FILEPATH="/oc/local/secops/jellyfish2/"
-JSONSCHEMA_DEFAULT_SCHEMA=JSONSCHEMA_FILEPATH+"/jellyfish_storage.json.schema"
+# Schema Parser
+import jsonschema
+
+JSONSCHEMA_FILEPATH = "/oc/local/secops/jellyfish2/"
+JSONSCHEMA_DEFAULT_SCHEMA = JSONSCHEMA_FILEPATH+"/jellyfish_storage.json.schema"
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	# -h help
-	# -s Audit Definitions (Required)
-	# -j JSON File
+    # -h help
+    # -s Audit Definitions (Required)
+    # -j JSON File
 
-	parser.add_argument("-s", "--schema", help="JSON Schema File to use for validation. (Default : jellyfish_storage.json.schema)", default=JSONSCHEMA_DEFAULT_SCHEMA )
-	parser.add_argument("-j", "--json", help="JSON File to Validate", required="TRUE")
+    parser.add_argument("-s", "--schema", help="JSON Schema File to use for validation. (Default : jellyfish_storage.json.schema)", default=JSONSCHEMA_DEFAULT_SCHEMA)
+    parser.add_argument("-j", "--json", help="JSON File to Validate", required="TRUE")
 
-	args=parser.parse_args()
+    args = parser.parse_args()
 
-	schema_file=args.schema
-	json_file=args.json
+    schema_file = args.schema
+    json_file = args.json
 
 def storageJSONVerify(schema_file, json_file):
 
-	if type(json_file) is dict:
-		# Treat this as the dict itself
-		this_json = json_file
-	else:
-		try:
-			with open(json_file,"r") as this_json_file:
-				this_json = json.load(this_json_file)
-		except ValueError as err:
-			msg="Error in the Format of your JSON File " + err
-			return (False, msg)
+    '''
+    Using the jsonschema file specified check the json bits for compliance
+    '''
 
-	#print(this_json)
+    if isinstance(json_file, dict):
+        # Treat this as the dict itself
+        this_json = json_file
+    else:
+        try:
+            with open(json_file, "r") as this_json_file:
+                this_json = json.load(this_json_file)
+        except ValueError as err:
+            msg = "Error in the Format of your JSON File " + err
+            return (False, msg)
 
-	if type(schema_file) is dict :
-		# Treat this as the schema itself
-		this_schema = schema_file
-	else:
-		try:
-			with open(schema_file,"r") as this_schema_file:
-				this_schema = json.load(this_schema_file)
-		except ValueError as err:
-			msg="Error in the Format of your Schema File: " + str(err)
-			return (False, msg)
+    if isinstance(schema_file, dict):
+        # Treat this as the schema itself
+        this_schema = schema_file
+    else:
+        try:
+            with open(schema_file, "r") as this_schema_file:
+                this_schema = json.load(this_schema_file)
+        except ValueError as err:
+            msg = "Error in the Format of your Schema File: " + str(err)
+            return (False, msg)
 
-
-	#print(this_schema)
-
-	try:
-		jsonschema.validate(this_json,this_schema)
-	except jsonschema.exceptions.ValidationError as err:
-		msg="Error in your JSON File: " + err
-		return (False, msg)
-	except jsonschema.exceptions.SchemaError as err:
-		msg="Error with your Schema File: " + err
-		return (False, msg)
-	else:
-		msg="JSON file passed Schema Validation."
-		return (True, msg)
+    try:
+        jsonschema.validate(this_json, this_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        msg = "Error in your JSON File: {}".format(err)
+        return (False, msg)
+    except jsonschema.exceptions.SchemaError as err:
+        msg = "Error with your Schema File: {}".format(err)
+        return (False, msg)
+    else:
+        msg = "JSON file passed Schema Validation."
+        return (True, msg)
 
 if __name__ == "__main__":
-	#"We're going to run the main stuff
-	result = storageJSONVerify(schema_file, json_file)
+    #"We're going to run the main stuff
+    result = storageJSONVerify(schema_file, json_file)
 
-	print(result)
+    print(result)
--- subtype_large_compare.py
+++ subtype_large_compare.py
@@ -15,7 +15,7 @@ import pprint
 import pymysql
 import json
 import re
-import apt_pkg
+#import apt_pkg
 from copy import deepcopy
 from time import time
 from time import sleep
@@ -26,36 +26,36 @@ import multiprocessing
 from queue import Queue
 
 def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalue, FRESH):
-	
+
 	# Subtype Large Comparison
 
 	cur = db_conn.cursor()
-	
+
 	results_dict = deepcopy(host_list_dict)
-	
+
 	# Host Lists
 	host_ids_list = list()
 	fail_hosts = list()
 	success_hosts = list()
 	exempt_hosts = list()
-	
+
 	inserts = 0
 	updates = 0
-	
+
 	# Host ID
 	for item in host_list_dict :
 		host_ids_list.append(item['host_id'])
-		
+
 	# Massaging Types
-	
+
 	massaged_ctype = []
 	massaged_csubtype = []
 	massaged_mvalue = []
-	
+
 	# In order for each one. Rehydrate string (with ") and then convert to a tuple
-	
-	
-	
+
+
+
 	if type(ctype) is str :
 		massaged_ctype.append(ctype)
 	else :
@@ -68,25 +68,25 @@ def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalu
 	else :
 		# Cycle through each subtype list and toss that into csubtype value
 		massaged_csubtype = [ ast.literal_eval('"' + item.replace("," , "\",\"") + '"') for item in csubtype ]
-	
+
 	if type(mvalue) is str :
 		interm_processed_mvalue = ast.literal_eval('"' + mvalue.replace("," , "\",\"") + '"')
 		massaged_mvalue.append(interm_processed_mvalue)
 	else :
 		# Cycle throught the regexp matches and toss that into csubtype value
 		massaged_mvalue = [ ast.literal_eval('"' + item.replace("," , "\",\"") + '"') for item in mvalue ]
-	
+
 	#print(type(massaged_ctype), massaged_ctype)
 	#print(type(massaged_csubtype), massaged_csubtype)
 	#print(type(massaged_mvalue), massaged_mvalue)
-		
+
 	host_id_list_string = ",".join(map(str, host_ids_list))
-	
+
 	#print(type(host_id_list_string), host_id_list_string)
 	#print("About to build Queries")
-	
-	for index_value in range(0, len(massaged_ctype)) : 
-		
+
+	for index_value in range(0, len(massaged_ctype)) :
+
 		if mtype == "subnonhere" :
 			# Both do the same thing. They just accept either a numerical match or a zero
 			COMBINE = " OR "
@@ -100,10 +100,10 @@ def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalu
 			COLUMNMATCH = " != "
 		else :
 			raise Exception("Unknown match type ", mtype)
-		
+
 		#print(COMBINE, COLUMNMATCH)
 		# Cycle through each ctype & Do a Collection
-		
+
 		collection = []
 		collection.append("SELECT fk_host_id, count(DISTINCT(collection_subtype))")
 		collection.append("from collection")
@@ -114,42 +114,42 @@ def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalu
 		collection.append("	( collection_type = '" + str(massaged_ctype[index_value]) + "' )")
 		collection.append(" AND ")
 		collection.append(" ( ")
-		
+
 		#print(collection)
-		
+
 		# Grab the Column
 		columns_only = []
-		
+
 		for sub_index_value in range(0, len(massaged_csubtype[index_value])) :
-	
-			
+
+
 			# Generate My Column Match String
 			if massaged_mvalue[index_value][sub_index_value] == "any" :
 				matchstring = ""
 			else :
 				matchstring = " AND collection_value REGEXP '" + massaged_mvalue[index_value][sub_index_value] + "'"
-				
+
 			#print("Column Match: ", matchstring)
-				
+
 			columnmatch_string = "collection_subtype " + COLUMNMATCH +  "'" + massaged_csubtype[index_value][sub_index_value] + "'"
-			
+
 			columns_only.append("( " + columnmatch_string + matchstring + " )")
-			
+
 			#print(columns_only)
-			
+
 		columns_only_string = COMBINE .join(columns_only)
-		
-		collection.append(columns_only_string)	
+
+		collection.append(columns_only_string)
 		collection.append(" ) ")
-		
+
 		collection.append(" and last_update >= now() - INTERVAL " + str(FRESH) + " SECOND " )
 		collection.append(" group by fk_host_id order by last_update desc ;" )
-		
+
 		collection_query = " ".join(collection)
-		
+
 		if len(host_ids_list) > 0 :
 			cur.execute(collection_query)
-			if cur.rowcount: 
+			if cur.rowcount:
 					query_results_list = cur.fetchall()
 			else :
 				# No Results
@@ -157,22 +157,22 @@ def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalu
 				query_results_list = []
 		else :
 			query_results_list = []
-			
+
 		#print(query_results_list)
 		query_results_list_index = [ host[0] for host in query_results_list ]
 		#print(query_results_list_index)
-		
+
 		exempthost = list()
 		passhost = list()
 		failhost = list()
-		
-		try: 
+
+		try:
 			if mtype == "subnonhere" :
 				# No exempt hosts. All hosts not in our results pass
 				exempthost = []
 				passhost = [ host for host in host_ids_list if host not in query_results_list_index ]
 				failhost = [ host[0] for host in query_results_list if host[1] > 0 ]
-			elif mtype == "suballhere" : 
+			elif mtype == "suballhere" :
 				# All the hosts that Aren't in our query Results Fail
 				exempthost = [ ]
 				# All the hosts in the results whose output matches exactly
@@ -189,8 +189,8 @@ def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalu
 				raise Exception("Unknown match type. Potential Race Condition!")
 		except Exception as e:
 			print("Error Doing Comparisons: ", e)
-			
-		
+
+
 		#print(exempthost, passhost, failhost)
 
 		for host in range(0,len(results_dict)):
@@ -208,11 +208,11 @@ def subtype_large_compare(db_conn, host_list_dict, mtype, ctype, csubtype, mvalu
 				elif results_dict[host]['host_id'] in passhost :
 					results_dict[host]['pfe'] = "pass"
 					results_dict[host]['pfevalue'] = "Subtype Comparison " + mtype + " Passed"
-				elif results_dict[host]['host_id'] in exempthost : 
+				elif results_dict[host]['host_id'] in exempthost :
 					results_dict[host]['pfe'] = "notafflicted"
 					results_dict[host]['pfevalue'] = "Subtype Comparison " + mtype + " Exempt"
 			except Exception as e :
 				print("Error trying to match items relating to host ", host, " : " , e )
-	
+
 	cur.close()
 	return results_dict
