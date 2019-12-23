a245423224581ea6bad7be56a09ec793a1aad45b
Chris Halbersma <chris@halbersma.us>
2019-12-22 20:39:25 -0800

    Audittools Breakout
    Rewrote some of the audittools items to make it easier to add new,
    future audits. Also added some new audits to the

---
+++ audittools/__init__.py
@@ -0,0 +1,5 @@
+from .audit_source import AuditSource
+from .verifyAudits import walk_auditd_dir, load_auditfile, verifySingleAudit, verifyAudits
+from .cve_class import mowCVE
+from .audits_usn import AuditSourceUSN
+from .ubuntu_cve import mowCVEUbuntu
