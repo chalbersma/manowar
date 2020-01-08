79f2d2e8f9a050a57443650596a515117894c105
Chris Halbersma <chris@halbersma.us>
2020-01-08 13:51:15 -0800

    db_setup.sh

--- travis/db_setup.sh
+++ travis/db_setup.sh
@@ -4,7 +4,7 @@ set -x
 
 ## Setup DB Schema
 sudo mysql -u root -e "show full processlist;"
-sudo mysql -u root -e "shwo variables;"
+sudo mysql -u root -e "show variables;"
 
 sudo bash -c "mysql -u root < echo create database manowar2"
 schema_success=$?
