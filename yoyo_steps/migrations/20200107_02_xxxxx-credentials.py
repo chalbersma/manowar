fbd915ce6ef2c691ff6d058e99f425c64a5725fd
Chris Halbersma <chris@halbersma.us>
2020-01-10 11:36:56 -0800

    In Travis Add Travis manwoard file

--- yoyo_steps/migrations/20200107_02_xxxxx-credentials.py
+++ yoyo_steps/migrations/20200107_02_xxxxx-credentials.py
@@ -6,11 +6,14 @@ Configuration of API Users
 
 import os
 import os.path
+
 import logging
+import yaml
 
 from configparser import ConfigParser
 
 from yoyo import step
+
 
 __depends__ = {"20200107_01_Gv0ql-initialize"}
 
@@ -53,6 +56,33 @@ logger = logging.getLogger("yoyo-credentials Step")
 
 logger.info("Finding Configuration File yoyo.ini")
 
+LOGGER.debug("No Config File Given Let's Look in Default Locations.")
+
+possible_config_files =  ["/etc/manowar/manoward.yaml", "./etc/manowar/manoward.yaml", "/usr/local/etc/manowar/manoward.yaml"]
+
+if os.environ.get("TRAVIS", None) is not None:
+    logger.info("In a Travis Build Add the Travis Paths to Configuration.")
+    possible_config_files.append("./travis/artifacts/manoward.yaml")
+
+
+for default_file in possible_config_files:
+    if os.path.isfile(default_file) and os.access(default_file, os.R_OK):
+        LOGGER.debug("Using Default File : {}".format(default_file))
+
+        try:
+            with open(default_file, "r") as manoward_config_file:
+                manwoard_configs = yaml.safe_load(manoward_config_file)
+        except Exception as manoward_config_error:
+            logger.error("Unable to Read Manoward Configuration.")
+            logger.debug("Error : {}".format(manoward_config_error))
+
+            raise manoward_config_error
+        else:
+            # I've now Got my Things
+            logger.info("Found and loaded manoward.yaml")
+
+
+
 do_api_attempt = True
 config_file = None
 
@@ -64,65 +94,43 @@ password = None
 
 steps = list()
 
-for default_file in ("../yoyo.ini", "./yoyo.ini"):
-    if os.path.isfile(default_file) and os.access(default_file, os.R_OK):
-        logger.debug("Using Default File : {}".format(default_file))
-        config_file = default_file
-        break
+# DB Config Items
+yoyo_config = manoward_configs["database"]
 
-if config_file is None:
-    logger.warning("Yoyo applied without a configuration file not attempting to update credential stuff.")
-    raise ValueError("No yoyo configuration file given.")
-else:
-    # Load Configuration
-    try:
-        # Read Our INI with our data collection rules
-        config = ConfigParser()
-        config.read(config_file)
-    except Exception as general_config_error:
-        logger.warning("Couldn't read configuration file {} properly. Not doing api credential stuff.")
-        raise general_config_error
+for user_type in ["api", "store", "analyze"]:
+
+    if yoyo_config.get("manowar", dict()).get("{}_password".format(user_type), None) is  None:
+        # We not doing it
+        do_api_attempt = False
     else:
-        # DB Config Items
-        yoyo_config = dict(config._sections)
+        username = yoyo_config.get("manowar", dict()).get("{}_dbusername".format(user_type), None)
+        hostmask = yoyo_config.get("manowar", dict()).get("{}_hostmask".format(user_type), "%")
+        password = yoyo_config.get("manowar", dict()).get("{}_dbpassword".format(user_type), None)
 
-        for user_type in ["api", "store", "analyze"]:
+        req_enc = yoyo_config.get("manowar", dict()).get("req_enc", "SSL")
 
-            if yoyo_config.get("manowar", dict()).get("{}_password".format(user_type), None) is  None:
-                # We not doing it
-                do_api_attempt = False
+
+        this_u = '"{0}"@"{1}"'.format(username, hostmask)
+
+        steps.append(step("create or replace user {0} identified by \"{1}\"".format(this_u,
+                                                                                    password),
+                            "drop user {}".format(this_u)))
+
+        if req_enc in ("SSL", "NONE", "X509"):
+            steps.append(step("alter user {0} REQUIRE {1}".format(this_u,
+                                                                    req_enc)))
+        else:
+            raise ValueError("req_enc not set to valid entry")
+
+        # Now Add Permissions
+        for this_table in _table_defs.keys():
+            if user_type in _table_defs[this_table].keys():
+                logger.info("Adding rights to {} for {}".format(this_table, user_type))
+
+                for right in _table_defs[this_table][user_type]:
+
+                    steps.append(step("grant {} on {} to {}".format(right,
+                                                                    this_table,
+                                                                    this_u)))
             else:
-                username = yoyo_config.get("manowar", dict()).get("{}_username".format(user_type), None)
-                hostmask = yoyo_config.get("manowar", dict()).get("{}_hostmask".format(user_type), None)
-                password = yoyo_config.get("manowar", dict()).get("{}_password".format(user_type), None)
-
-                req_enc = yoyo_config.get("manowar", dict()).get("req_enc", "SSL")
-
-
-                this_u = '"{0}"@"{1}"'.format(username, hostmask)
-
-                steps.append(step("create or replace user {0} identified by \"{1}\"".format(this_u,
-                                                                                            password),
-                                  "drop user {}".format(this_u)))
-
-                if req_enc in ("SSL", "NONE", "X509"):
-                    steps.append(step("alter user {0} REQUIRE {1}".format(this_u,
-                                                                          req_enc)))
-                else:
-                    raise ValueError("req_enc not set to valid entry")
-
-                # Now Add Permissions
-                for this_table in _table_defs.keys():
-                    if user_type in _table_defs[this_table].keys():
-                        logger.info("Adding rights to {} for {}".format(this_table, user_type))
-
-                        for right in _table_defs[this_table][user_type]:
-
-                            steps.append(step("grant {} on {} to {}".format(right,
-                                                                            this_table,
-                                                                            this_u)))
-                    else:
-                        logger.debug("User {} Not granted access to {}".format(user_type, this_table))
-
-    finally:
-        pass
+                logger.debug("User {} Not granted access to {}".format(user_type, this_table))
