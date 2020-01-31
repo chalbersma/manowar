90b0cba9aae0819f32695e00bbfcb7d1039dd336
Chris <chris@halbersma.us>
2020-01-31 12:27:22 -0800

    Travis Env all IPs are Private IPs.

--- travis/schedule_test.sh
+++ travis/schedule_test.sh
@@ -52,28 +52,3 @@ else
 	echo -e "test_hostcollection.sql didn't work properly. Please investigate travis configuration."
 	exit 1
 fi
-
-ip_intel_test="$(mktemp /tmp/ipintel.XXXXX)"
-
-# Test that it landed properly. Grab the results for host #1
-sudo bash -c "mysql manowar2 -u root < travis/artifacts/test_ipintel.sql > ${ip_intel_test} "
-
-didit_ipintel=$?
-
-if [[ ${didit_ipintel:-0} -eq 0 ]] ; then
-	# If Worked
-	results_found=$(wc -l < "${ip_intel_test}")
-	if [[ ${results_found:-0} -gt 0 ]] ; then
-		# There are results
-		echo -e "Testing host 1, found ${results_found} Intel Entries. Looks Good"
-		echo -e "Debug info for host. Printing IP Intel"
-		cat "${ip_intel_test}"
-	else
-		# Didn't Find any results
-		echo -e "Testing host 1, found no intel. This is an error."
-		exit 1
-	fi
-else
-	echo -e "test_ipintel.sql didn't work properly. Please investigate travis configuration."
-	exit 1
-fi
